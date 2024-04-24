ee3ea6af7372b12adfe775194b9816eced745ffb
kotaYkw <yukawa.kota.yj@alumni.tsukuba.ac.jp>
2024-04-25 00:17:21 +0900

    🐛  delete script

--- git_delete.sh
+++ git_delete.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# 現在のディレクトリから開始して、すべての.gitディレクトリを削除する
-remove_git_dirs() {
+# サブディレクトリ内のすべての.gitディレクトリと.gitignoreファイルを削除する
+remove_git_dirs_and_gitignore() {
     # カレントディレクトリ内のすべてのディレクトリを取得
     dirs=(*/)
 
-    # カレントディレクトリ内の.gitディレクトリを削除
-    if [ -d ".git" ]; then
-        rm -rf .git
-        echo "Removed .git directory from $(pwd)"
-    fi
-
-    # 各サブディレクトリに対して再帰的に関数を呼び出す
+    # 各サブディレクトリに対して処理を行う
     for dir in "${dirs[@]}"; do
         cd "$dir"
-        remove_git_dirs
+
+        # .gitディレクトリを削除
+        if [ -d ".git" ]; then
+            rm -rf .git
+            echo "Removed .git directory from $(pwd)"
+        fi
+
+        # .gitignoreファイルを削除
+        if [ -f ".gitignore" ]; then
+            rm .gitignore
+            echo "Removed .gitignore file from $(pwd)"
+        fi
+
+        # サブディレクトリに対して再帰的に関数を呼び出す
+        remove_git_dirs_and_gitignore
         cd ..
     done
 }
 
 # メイン処理
-remove_git_dirs
+remove_git_dirs_and_gitignore
